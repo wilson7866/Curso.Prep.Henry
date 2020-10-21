3a80c816f097c9b095a3f7d3028e08c1e60c14f1
wilson7866 <wilsondelgado@hotmail.es>
2020-10-20 21:45:25 -0500

    start

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var objeto= {
+    nombre: 'nombre',
+    edad: 'edad',
+    meow: function(){
+      return 'Meow!';
+    }
+  };
+  return objeto;
 }
 
 
@@ -14,6 +22,9 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto.property= null;
+  return objeto;
+
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,6 +32,12 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  var objeto= {
+  metodo: function(){
+
+  }
+};
+objeto.metodo();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
